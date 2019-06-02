277aa8eaa6ba7f57d89f7672b6c1f37312ac049d
Han Xu <hanxu27@gmail.com>
2019-06-01 21:47:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(input){
   // Write your code here!
+  if (input <= 400) {
+    return "This one is on me!";
+  } else if (input > 2000 && input <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (input > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(input){
   // Write your code here!
+  return input === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(input){
   // Write your code here!
+  switch (input) {
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
