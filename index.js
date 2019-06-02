9edbdc12fdc3090d07462b94b5960992632dec9a
Andrew Allen <asallen67@gmail.com>
2019-06-02 16:03:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if (num > 2500)
+  	return 'No can do.';
+  if (num > 2000)
+  	return 'I will gladly take your thirty bucks.';
+  if (num <= 400)
+  	return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == 'NYC')
+  	return 'Ok, sounds good.'
+  else
+  	return 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip == 'generous')
+  	return 'Thank you so much.'
+  if (tip == 'not as generous')
+  	return 'Thank you.'
+  else
+  	return 'Bye.'
 }
